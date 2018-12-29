af132706da747eae0e5281deadc698bdafa80164
Nicholas <nlediet@northern.co>
2018-12-29 11:29:37 -0500

    Add recommended word

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import json
 import difflib
 
-from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("dictionary.json"))
 
 def retrive_def(word):
-    return data[word]
+    word = word.upper()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return ("Did you mean %s instead?" % get_close_matches(word, data.keys())[0])
+    else:
+        return ("The Word doesn't exist, please double check it.")
 
-word_user = input("Enter a word:\s")
 
-print(retrive_def(word_user.upper()))
+word_user = input("Enter a word:\n")
+
+print(retrive_def(word_user))
